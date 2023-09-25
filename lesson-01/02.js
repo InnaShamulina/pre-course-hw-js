3cfd957fe89271718f7ff1db709d9c342b9e2f19
InnaShamulina <innaaf@yandex.ru>
2023-09-25 16:22:36 +0300

    task 2, random number

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,4 @@
 // code
+let number1 = Math.floor(Math.random() * 100)
+let even = number1 % 2 
+even === 0 ? console.log(`${number1} :Это четное число`) : console.log(`${number1} :Это нечетное число`);
